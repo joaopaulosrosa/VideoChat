287a682f6f99b434fa1a5117cc700f21dc49e0a3
joao rosa <joaopaulosrosa@gmail.com>
2022-03-16 16:27:10 -0300

    javascript to remove user from DOM if left

--- static/js/streams.js
+++ static/js/streams.js
@@ -10,6 +10,7 @@ let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
   client.on('user-published', handleUserJoined)
+  client.on('user-left', handleUserLeft)
 
   UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
@@ -49,6 +50,11 @@ let handleUserJoined = async (user, mediaType) => {
   if(mediaType === 'audio'){
     user.audioTrack.play()
   }
+}
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid]
+  document.getElementById(`user-container-${user.uid}`).remove()
 }
 
 joinAndDisplayLocalStream()
